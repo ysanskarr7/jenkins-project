2adb049df5799744cc80c2be70e686b742ebcfc7
Sanskar Yadav <ysanskarr7@gmail.com>
2026-07-09 17:37:15 +0530

    Fix user command and add nginx installation
    Corrected command to get the current user and added installation steps for nginx.

--- hello.sh
+++ hello.sh
@@ -1,6 +1,9 @@
 echo "Welcome to the Jenkins Hell 😒"
-echo "User : $(whoiam)"
+echo "User : $(whoami)"
 echo "Hostname : $(hostname)"
 echo "Current Directory : $(pwd) "
 echo "Date : $date"
+echo "Installing Updates"
 sudo apt update
+sudo apt install -y nginx
+nginx --version
